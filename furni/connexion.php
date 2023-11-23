77901a00a205188116c79fce2048b760d97890be
Kanisse <73669714+Kanisse@users.noreply.github.com>
2023-11-23 12:09:00 +0100

    init

---
+++ furni/connexion.php
@@ -0,0 +1,62 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Untree.co">
+  <link rel="shortcut icon" href="favicon.png">
+
+  <meta name="description" content="" />
+  <meta name="keywords" content="bootstrap, bootstrap4" />
+
+		<!-- Bootstrap CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+		<link href="css/tiny-slider.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
+	</head>
+
+    <body>
+
+
+<form>
+                <div class="row">
+                  <div class="col-4"></div>
+                  <div class="col-4">
+                  <div class="form-group">
+                  <label class="text-black" for="email">Email address</label>
+                  <input type="email" class="form-control" id="email">
+                </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-4"></div>
+                  <div class="col-4">
+                  <div class="form-group mb-5">
+                  <label class="text-black" for="message">Mot de passe</label>
+                  <input name="" class="form-control" id="message">
+                </div>
+                </div>
+                  </div>
+
+                  <div class="row">
+                  <div class="col-4"></div>
+                  <div class="col-4">
+                  <button type="submit" class="btn btn-primary-hover-outline">Send Message</button>
+              </form>
+                </div>
+                  </div>
+
+                </div>
+
+                
+
+                
+
+                
+
+    </body>
